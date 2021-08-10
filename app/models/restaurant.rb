f77aa264bd26cbdc4b4796b6b53fb8444d4da970
Khamza Habibullah <habibullahkhamza@gmail.com>
2021-08-10 14:24:36 +0400

    working app

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,8 @@
 class Restaurant < ApplicationRecord
-  CATEGORY = ['chinese', 'italian', 'japanese', 'french', 'belgian']
+  CATEGORY = ['Chinese', 'Italian', 'Japanese', 'French', 'Belgian']
   validates :name, presence: true
   validates :address, presence: true
-  validates :category, presence: true
-  validates :category, inclusion: { in: CATEGORY }
+  validates :category, presence: true, inclusion: { in: CATEGORY }
+  # validates :category,
   has_many :reviews, dependent: :destroy
 end
